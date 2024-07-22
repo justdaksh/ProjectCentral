dc1c05baa36ffbe07b8be9b61a68609b24b60105
justdaksh <sdaksh31@gmail.com>
2024-07-22 21:36:42 +0530

    Enabling Speed-Insights React@vercel

--- ProjectCentral/src/main.jsx
+++ ProjectCentral/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import CssBaseline from '@mui/material/CssBaseline';
 import {BrowserRouter} from 'react-router-dom'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
