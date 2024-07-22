dbe3236b4d0936a1d5118a7d30818769099f4094
justdaksh <sdaksh31@gmail.com>
2024-07-22 21:32:05 +0530

    Enabling Speed Insights for Vercel

--- ProjectCentral/src/main.jsx
+++ ProjectCentral/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import CssBaseline from '@mui/material/CssBaseline';
 import {BrowserRouter} from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssBaseline/>
     <BrowserRouter>
         <App />
+        <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>,
 )
